aae788f6db303c0126c9ab0e072eecb310a9eeb0
Linda-404 <Linda-404@users.noreply.github.com>
2023-03-08 15:41:49 +0100

    delete delay animation on about page

--- pages/about.js
+++ pages/about.js
@@ -23,7 +23,7 @@ export default function About() {
           <motion.div
             initial={{ y: 50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.75, delay: 0.2 }}
+            transition={{ duration: 0.75 }}
             className="heading_container"
           >
             <h1>
